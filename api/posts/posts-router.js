13c3cf85952b2dc9edf451c99b788d453d351ac2
sebastian espeset <sebastian.c.espeset@gmail.com>
2021-02-09 16:39:38 -0600

    fetching specific post functioning

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -11,6 +11,7 @@ const router = express.Router();
 
 //build out the endpoints
 
+//1. basic data retrieval
 router.get('/',(req,res)=>{
     Posts.find(req.query)
         .then(posts =>{
@@ -19,6 +20,20 @@ router.get('/',(req,res)=>{
         .catch((error)=>{
             res.status(400).json({message:`${error}`})
         })
+})
+
+//2.fetching specific post via id
+router.get('/:id', (req,res)=>{
+    Posts.findById(req.params.id)
+        .then(post=>{
+            if(post){
+                res.status(200).json(post)
+            } else{
+                res.status(404).json({message:`The post with the specified ID does not exist`})
+            }
+        }) .catch(error=>{
+            res.status(500).json({message:`${error}`})
+        });
 })
 
 module.exports = router;
