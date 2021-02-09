e8abee2824b71843d056a47c95b2a8b8f9d38ec9
sebastian espeset <sebastian.c.espeset@gmail.com>
2021-02-09 16:33:28 -0600

    finally retrieving data correctly.

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,24 @@
 // implement your posts router here
+
+//import models
+const Posts = require('./posts-model');
+
+//import express
+const express = require("express");
+
+//import Router
+const router = express.Router();
+
+//build out the endpoints
+
+router.get('/',(req,res)=>{
+    Posts.find(req.query)
+        .then(posts =>{
+            res.status(200).json(posts)
+        })
+        .catch((error)=>{
+            res.status(400).json({message:`${error}`})
+        })
+})
+
+module.exports = router;
