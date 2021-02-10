c0b5ae1f7b14ec3205cf923de6e01ef279e7cb31
sebastian espeset <sebastian.c.espeset@gmail.com>
2021-02-09 19:19:58 -0600

    delete functionality added

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -65,6 +65,21 @@ router.put('/:id',(req,res)=>{
         }) .catch((error)=>{
             res.status(404).json({message:`error updating`})
         })
+});
+
+//5. Delete a post
+router.delete('/:id',(req,res)=>{
+    const postId = req.params.id;
+    Posts.remove(postId)
+        .then(post=>{
+            if(!postId){
+                res.status(404).json({message:`The post with the specified ID does not exist`})
+            } else{
+                res.status(200).json(`Post with id: ${postId} was successfully deleted`)
+            }
+        }) .catch(error=>{
+            res.status(500).json({message:`The post could not be removed`})
+        })
 })
 
 module.exports = router;
